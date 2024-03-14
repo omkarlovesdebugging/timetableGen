a615248d01332b5ca079efbdf2b63f47e6b3e63f
OMKAR SUDHIR GHOLAP <142476850+omkarlovesdebugging@users.noreply.github.com>
2024-03-14 15:20:28 +0530

    database extended

--- database.py
+++ database.py
@@ -3,17 +3,12 @@ import sqlite3
 conn = sqlite3.connect('Timetable_Generator.db')
 
 c = conn.cursor()
-
-conn.execute("""
-  CREATE TABLE teacher (
-    teacher_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    first_name VARCHAR(45) NOT NULL,
-    last_name VARCHAR(45) NOT NULL,
-    subject_name VARCHAR(45) NOT NULL,
-    email VARCHAR(45) NOT NULL);
-""")
-
 conn.commit()
-
+# CREATE TABLE teacher (
+#     teacher_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     first_name VARCHAR(45) NOT NULL,
+#     last_name VARCHAR(45) NOT NULL,
+#     subject_name VARCHAR(45) NOT NULL,
+#     email VARCHAR(45) NOT NULL);
 print('Connnection successful')
 conn.close()
