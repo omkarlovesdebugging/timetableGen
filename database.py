858fa0f1e757db500bd5825c1e2f7de55fdeba9b
OMKAR SUDHIR GHOLAP <142476850+omkarlovesdebugging@users.noreply.github.com>
2024-03-14 14:33:36 +0530

    corrected database code

--- database.py
+++ database.py
@@ -5,7 +5,12 @@ conn = sqlite3.connect('Timetable_Generator.db')
 c = conn.cursor()
 
 conn.execute("""
-
+  CREATE TABLE teacher (
+    teacher_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name VARCHAR(45) NOT NULL,
+    last_name VARCHAR(45) NOT NULL,
+    subject_name VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL);
 """)
 
 conn.commit()
