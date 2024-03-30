8bf62a8c2ccf41a8037f67ad7ccdd2262d1e3e93
ogXmessi <142476850+omkarlovesdebugging@users.noreply.github.com>
2024-03-31 01:46:39 +0530

    kuch nahi

--- library.py
+++ library.py
@@ -1,17 +1,8 @@
-from tkinter import *
+import random 
+list = [ "omkar","Sudhir","gholap","Chotu"]
 
-class TimetableGenerator:
-    def __init__(self,root):
-        self.root=root
-        self.root.title("Timetable Generator")
-        self.root.geometry("1520x800+0+0")
-
-        lbltitle=Label(self.root,text="Automated Timetable Generator",bg="cyan",fg="black",bd=20,relief=RIDGE,font=("times new roman",50,"bold"),padx=2,pady=6)
-        lbltitle.pack(side=TOP,fill=X)
-
-
-
-if __name__=="__main__":
-    root=Tk()
-    obj=TimetableGenerator(root)
-    root.mainloop()
+for i in list :
+    random.shuffle(list)
+    if i=="gholap":
+        i="omkar"
+    print(i)
