86e711d8627120f4f9443db70de8ed96c4a59839
ronak03rsk <2022.ronak.katariya@ves.ac.in>
2024-04-09 16:31:32 +0530

    tried to divide the lab according to the batches

--- timetableRK.py
+++ timetableRK.py
@@ -227,19 +227,11 @@ def save_details():
         msg = messagebox.showerror("ERROR","Please generate a TT for all the classes first") 
         return ValueError
 
-    # with open("TIMETABLE_GEN.txt", "w") as file:
-    #     file.write("TIMETABLE GENERATOR\n")
-    #     file.write("\nD10A:\n")
-    #     for i in data_a:
-    #         file.write(f"{i}\n")
-    #     file.write("\nD10B:\n")
-    #     for j in data_b:
-    #         file.write(f"{j}\n")
-    #     file.write("\nD10C:\n")
-    #     for k in data_c:
-    #         file.write(f"{k}\n")
 
-"""ye wala vo back to back free lecture wala hai """
+
+
+import random
+
 def fill_random_tt():
     save_data.clear()
     show_focusA(event=None)
@@ -252,84 +244,89 @@ def fill_random_tt():
     for i in data:
         teacher_fullname = i[1] + " " + i[2]
         subject_fullname = i[3]
-        room="514"
-
+        room = "514"
         formatted_data_A.append([teacher_fullname, subject_fullname, room, 0])
-    
+
     time_slots = ["8:30-9:30", "9:30-10:30", "10:30-11:30", "11:30-12:30", "BREAK", "1:30-3:30"]
     days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-    
+
     save_data.clear()
 
     total_free_lecture_count = 0
     total_nonfree_lecture_count = 0
+
+    lab_subjects = set()  # Set to keep track of lab subjects for each day
 
     for day in days:
         free_lecture_count_per_day = 0
         non_free_lecture_count_per_day = 0
         last_was_free = False  # Variable to keep track of the last lecture
 
+        # Reset lab count and subjects for each day
+        lab_count_per_day = 0  
+        lab_subjects.clear()
+
         for slot in time_slots:
             if slot != "BREAK":
                 random.shuffle(formatted_data_A)
-                if (formatted_data_A[0][3] < 3 and non_free_lecture_count_per_day < 4):
-                    data=[formatted_data_A[0][0],formatted_data_A[0][1],formatted_data_A[0][2], day, slot]
-                    formatted_data_A[0][3] += 1
-                    non_free_lecture_count_per_day += 1
-                    total_nonfree_lecture_count += 1
-                    last_was_free = False
-                    save_data.append(data)
-                elif (free_lecture_count_per_day < 3 or non_free_lecture_count_per_day > 3) and total_free_lecture_count < 10:
-                    data=["Free", "", "", day, slot]
-                    free_lecture_count_per_day += 1
-                    total_free_lecture_count += 1
-                    last_was_free = True
-                    save_data.append(data)
-                else:
-                    if (total_free_lecture_count < 10):
-                        data=["Free", "", "", day, slot]
-                        free_lecture_count_per_day += 1
-                        total_free_lecture_count += 1
-                        last_was_free = True
+                
+                # Handling labs
+                if slot.startswith("1:30") and lab_count_per_day < 3:
+                    lab_subject = "Lab" + str(lab_count_per_day + 1)
+                    room = "Lab" + str(lab_count_per_day + 1)
+                    lab_count_per_day += 1
+
+                    # Ensure the lab subject is unique for the day
+                    while lab_subject in lab_subjects:
+                        lab_subject = "Lab" + str(lab_count_per_day + 1)
+                        room = "Lab" + str(lab_count_per_day + 1)
+
+                    lab_subjects.add(lab_subject)
+
+                    # Assign lab sessions to batches A, B, and C
+                    for batch in ['A', 'B', 'C']:
+                        lab_teacher = formatted_data_A.pop(0)
+                        data = [lab_subject + ' - Batch ' + batch, lab_teacher[0], room, day, slot]
                         save_data.append(data)
-                    else:
-                        # If last lecture was free, prioritize a non-free lecture
-                        if last_was_free:
-                            for i in formatted_data_A:
-                                if i[3] < 3:
-                                    data=[i[0],i[1],i[2], day, slot]
-                                    i[3] += 1
-                                    save_data.append(data)
-                                    non_free_lecture_count_per_day += 1
-                                    total_nonfree_lecture_count += 1
-                                    last_was_free = False
-                                    break
-                        # If last lecture was non-free, prioritize a free lecture
-                        else:
-                            data=["Free", "", "", day, slot]
-                            free_lecture_count_per_day += 1
-                            total_free_lecture_count += 1
-                            last_was_free = True
+
+                # Handling regular lectures
+                else:
+                    if formatted_data_A:  # Ensure there are teachers left
+                        if formatted_data_A[0][3] < 3 and non_free_lecture_count_per_day < 4:
+                            data = [formatted_data_A[0][1], formatted_data_A[0][0], formatted_data_A[0][2], day, slot]
+                            formatted_data_A[0][3] += 1
+                            non_free_lecture_count_per_day += 1
+                            total_nonfree_lecture_count += 1
+                            last_was_free = False
                             save_data.append(data)
 
-    if (len(formatted_data_A) < 5 and len(formatted_data_A) > 0):
-        msg = messagebox.showerror("ERROR","You must have atleast 5 teachers to generate a valid TT") 
+                        else:  # If no more non-free lectures available, add a free lecture
+                            if free_lecture_count_per_day < 3 or non_free_lecture_count_per_day > 3:
+                                data = ["Free", "", "", day, slot]
+                                free_lecture_count_per_day += 1
+                                total_free_lecture_count += 1
+                                last_was_free = True
+                                save_data.append(data)
+
+    if len(formatted_data_A) < 5:
+        msg = messagebox.showerror("ERROR", "You must have at least 5 teachers to generate a valid TT")
         return ValueError
 
     cursor.execute("""delete from timetable""")
     conn.commit()
 
-    for i in range(0,len(save_data)):
-        cursor.execute("INSERT INTO timetable (subject_name, teacher_name, room_number, day, time_slot) VALUES ( ?, ?, ?, ?, ?)", (save_data[i][0],save_data[i][1],save_data[i][2], save_data[i][3], save_data[i][4]))
+    for item in save_data:
+        cursor.execute("INSERT INTO timetable (subject_name, teacher_name, room_number, day, time_slot) VALUES (?, ?, ?, ?, ?)", item)
         conn.commit()
 
     timetable_frame(subjects=save_data)
-   
 
 
 
 
 
+
+"""ye wala vo back to back free lecture wala hai """
 # def fill_random_tt():
 #     save_data.clear()
 #     show_focusA(event=None)
@@ -346,7 +343,6 @@ def fill_random_tt():
 
 #         formatted_data_A.append([teacher_fullname, subject_fullname, room, 0])
     
-#     # print(formatted_data_B)
 #     time_slots = ["8:30-9:30", "9:30-10:30", "10:30-11:30", "11:30-12:30", "BREAK", "1:30-3:30"]
 #     days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
     
@@ -358,6 +354,7 @@ def fill_random_tt():
 #     for day in days:
 #         free_lecture_count_per_day = 0
 #         non_free_lecture_count_per_day = 0
+#         last_was_free = False  # Variable to keep track of the last lecture
 
 #         for slot in time_slots:
 #             if slot != "BREAK":
@@ -367,28 +364,40 @@ def fill_random_tt():
 #                     formatted_data_A[0][3] += 1
 #                     non_free_lecture_count_per_day += 1
 #                     total_nonfree_lecture_count += 1
+#                     last_was_free = False
 #                     save_data.append(data)
 #                 elif (free_lecture_count_per_day < 3 or non_free_lecture_count_per_day > 3) and total_free_lecture_count < 10:
 #                     data=["Free", "", "", day, slot]
 #                     free_lecture_count_per_day += 1
 #                     total_free_lecture_count += 1
+#                     last_was_free = True
 #                     save_data.append(data)
 #                 else:
 #                     if (total_free_lecture_count < 10):
 #                         data=["Free", "", "", day, slot]
 #                         free_lecture_count_per_day += 1
 #                         total_free_lecture_count += 1
+#                         last_was_free = True
 #                         save_data.append(data)
 #                     else:
-#                         for i in formatted_data_A:
-#                             if i[3] < 3:
-#                                 data=[i[0],i[1],i[2], day, slot]
-#                                 i[3] += 1
-#                                 # save_data.clear()
-#                                 save_data.append(data)
-#                                 non_free_lecture_count_per_day += 1
-#                                 total_nonfree_lecture_count += 1
-#                                 break
+#                         # If last lecture was free, prioritize a non-free lecture
+#                         if last_was_free:
+#                             for i in formatted_data_A:
+#                                 if i[3] < 3:
+#                                     data=[i[0],i[1],i[2], day, slot]
+#                                     i[3] += 1
+#                                     save_data.append(data)
+#                                     non_free_lecture_count_per_day += 1
+#                                     total_nonfree_lecture_count += 1
+#                                     last_was_free = False
+#                                     break
+#                         # If last lecture was non-free, prioritize a free lecture
+#                         else:
+#                             data=["Free", "", "", day, slot]
+#                             free_lecture_count_per_day += 1
+#                             total_free_lecture_count += 1
+#                             last_was_free = True
+#                             save_data.append(data)
 
 #     if (len(formatted_data_A) < 5 and len(formatted_data_A) > 0):
 #         msg = messagebox.showerror("ERROR","You must have atleast 5 teachers to generate a valid TT") 
@@ -397,18 +406,14 @@ def fill_random_tt():
 #     cursor.execute("""delete from timetable""")
 #     conn.commit()
 
-#     cursor.execute("""delete from timetable_B""")
-#     conn.commit()
-
-#     cursor.execute("""delete from timetable_C""")
-#     conn.commit()
-
 #     for i in range(0,len(save_data)):
 #         cursor.execute("INSERT INTO timetable (subject_name, teacher_name, room_number, day, time_slot) VALUES ( ?, ?, ?, ?, ?)", (save_data[i][0],save_data[i][1],save_data[i][2], save_data[i][3], save_data[i][4]))
 #         conn.commit()
 
-#     # load_timetable(subjects=formatted_data_A)
 #     timetable_frame(subjects=save_data)
+   
+
+
 
 
 def fill_timetable_A():
